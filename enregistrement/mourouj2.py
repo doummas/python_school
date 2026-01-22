a60db2ba9921ead5d2e404cdff168540a03d3b58
doummas <sammoud666@gmail.com>
2026-01-22 22:29:29 +0100

    oui

---
+++ enregistrement/mourouj2.py
@@ -0,0 +1,73 @@
+# t3 classe
+from numpy import array
+
+
+def saisir():
+    n=int(input("Donner n\n"))
+    while n<2 or n>20:
+        n=int(input("Donner n autre fois \n"))
+    return n
+
+
+def remplirT(T,n):
+    for i in range(n):
+        T[i] = dict()
+        T[i]["nom"]=input("Donner votre nom\n")
+        while verifier(T[i]["nom"])==False:
+            T[i]["nom"]=input("Donner votre nom autre fois\n")
+        T[i]["prenom"]=input("Donner votre prenom\n")
+        while verifier(T[i]["prenom"])==False:
+            T[i]["prenom"]=input("Donner votre prenom autre fois\n")
+        T[i]["id"] = int(input("Donner l'identiter\n"))
+        while unique(T[i]["id"],i,T) == False :
+            T[i]["id"] = int(input("Donner l'identiter autre fois\n"))
+        T[i]["age"] = int(input("Donner l'age \n"))
+        while T[i]["age"]>60 or T[i]["age"] <15:
+            T[i]["age"] = int(input("Donner l'age autre fois \n"))
+        T[i]["genre"] = (input("Donner genre\n")).upper()
+        while T[i]["genre"] != "F" and T[i]["genre"] !="M":
+            T[i]["genre"] = (input("M|F \n")).upper()
+        T[i]["année_embauche"] = int(input("Donner année_embauche\n"))
+        while not(1000<=T[i]["année_embauche"]<=9999):
+            T[i]["année_embauche"] = int(input("Donner année_embauche autre fois\n"))
+        
+
+def remplirT1(T,T1,n):
+    for i in range(n):
+        T1[i]["matricule"] = T[i]["nom"][0] + T[i]["prenom"][0] + str(T[i]["id"]) + T[i]["genre"] + str(T[i]["année_embauche"])[1:]
+        T1[i]["age"] = T[i]["age"]
+        print(T[i])
+        print(T1[i])
+
+def unique(en,n,T):
+    i=0
+    while i<n and T[i]["id"] != en:
+        i+=1
+    return i == n and len(str(T[i]["id"])) == 8
+        
+def verifier(ch):
+    ch=ch.upper()
+    i=0
+    while i<len(ch) and "A"<=ch[i]<="Z":
+        i+=1
+    return i == len(ch)
+
+
+em=dict(
+    nom=str,
+    prenom=str,
+    id=int,
+    age=int,
+    genre=str(),
+    année_embauche = int
+)
+
+mat=dict(
+    matricule = str,
+    age = int
+)
+n=saisir()
+T=array([em]*n)
+T1=array([mat]*n)
+remplirT(T,n)
+remplirT1(T,T1,n)
--- enregistrement/ras_fartass.py
+++ enregistrement/ras_fartass.py
@@ -1,37 +1,40 @@
+# t3 classe
 from numpy import array
+from math import *
 
-def saisir():
-    global n
-    n=int(input("Donner le taille \n "))
-    while n>40 or n<8:
-        n=int(input("Donner le taille \n "))
+
+def saisir ():
+    n=int(input("Donner le nombre des points \n"))
+    while n>30 or n<2:
+       n=int(input("Donner le nombre des points autre fois \n")) 
+    return n
 
 
 def remplir(T,n):
     for i in range(n):
-        T[i]["nom"] = input("Donner le nom d'eleve\n")
-        while valide(T[i],["nom"] == False):
-            T[i]["nom"] = input("Donner le nom d'eleve autre fois\n ")
-        T[i]["prenom"] = input("Donner le prenom\n")
-        while valide(T[i],["prenom"] == False):
-            T[i]["prenom"] = input("Donner le prenom d'eleve autre fois\n ")
-        T[i]["moyenne"] = float(input("Donner le moyenne\n"))
-        while T[i]["moyenne"] >20 or T[i]["moyenne"]<0:
-            T[i]["moyenne"] = float(input("Donner le moyenne autre fois\n"))
-def valide(ch):
-    ch=ch.upper()
-    i=0
-    while i<len(ch) and "A"<=ch[i]<="Z":
-        i=i+1
-    return i==len(ch) 
+        T[i] = dict()
+        T[i]["x"] = float(input(f"Donner x{i}\n"))
+        T[i]["y"] = float(input(f"Donner y{i}\n"))
+
+def nombre(T,n):
+    for i in range(n):
+        a=float(input(f"Donner a pour la point {i} "))
+        b=float(input(f"Donner b pour la point {i} "))
+        c=float(input(f"Donner c pour la point {i} "))
+        p=0
+        if a*T[i]["x"] + b*T[i]["y"] + c ==0:
+            p=p+1
+    print(f"Le nombre des point est {p}")
 
 
-eleve=dict(
-    nom = str,
-    prenom = str,
-    moyenne = float,
-    rang = int
 
+
+
+n=saisir()
+point=dict(
+    x=float,
+    y=float
 )
-
-T=array([eleve]*n)
+T=array([point]*n)
+remplir(T,n)
+nombre(T,n)
